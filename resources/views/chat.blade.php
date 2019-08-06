138661a8306902694de02f0fae72a97a35ca1d02
Thiago Galeno <thiagogaleno1301@hotmail.com>
2019-08-06 16:52:09 -0300

    inicio de layout do chat

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,14 @@
+<div class="chat">
+    <div class="card">
+        <div class="card-header bg-primary">
+        head
+        </div>
+        <div class="card-body">
+        body
+        </div>
+    </div>
+    <form>        
+        <input type="text" class="inChat"/>
+        <button class="btn btn-primary"> Enviar </button>        
+    </form>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,66 +1,61 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Desktopper</h1>
-    <!-- sites de diversão -->
-    <div class="fundo">
-        <div class="container-fluid">
-            <div class="row"> 
-                <div class="col text-center">
-                    <img src="images\youtube.png">
-                    <p>asdf1</p>
-                </div>
+@include('top')
 
-                <div class="col text-center">
-                    <img src="images\youtube.png">
-                    <p>asdf2</p>
-                </div>
-
-                <div class="col text-center">
-                    <img src="images\youtube.png">
-                    <p>asdf3</p>
-                </div>
-                <div class="col text-right">
-                    <img src="images\youtube.png">
-                    <p>asdf3</p>
-                </div>
+<h1>Desktopper</h1>
+<!-- sites de diversão -->
+<div class="fundo">
+    <div class="container-fluid">
+        <div class="row"> 
+            <div class="col text-center">
+                <img src="images\youtube.png">
+                <p>asdf1</p>
             </div>
-        </div>        
-    </div>
 
-    <!-- sites de aprendizado -->
-    <div class="fundo">
-        <div class="container-fluid">
-            <div class="row"> 
-                <div class="col text-center">
-                    <img src="images\udemy.jpg">
-                    <p>asdf1</p>
-                </div>
-
-                <div class="col text-center">
-                    <img src="images\udemy.jpg">
-                    <p>asdf2</p>
-                </div>
-
-                <div class="col text-center">
-                    <img src="images\udemy.jpg">
-                    <p>asdf3</p>
-                </div>
-                <div class="col text-right">
-                    <img src="images\udemy.jpg">
-                    <p>asdf3</p>
-                </div>
+            <div class="col text-center">
+                <img src="images\youtube.png">
+                <p>asdf2</p>
             </div>
-        </div>        
-    </div>
 
-    <link rel="stylesheet" href="css/style.css" type="text/css">
-    <link rel="stylesheet" href="css/app.css" type="text/css">
-</body>
-</html>
+            <div class="col text-center">
+                <img src="images\youtube.png">
+                <p>asdf3</p>
+            </div>
+            <div class="col text-right">
+                <img src="images\youtube.png">
+                <p>asdf3</p>
+            </div>
+        </div>
+    </div>        
+</div>
+
+<!-- sites de aprendizado -->
+<div class="fundo">
+    <div class="container-fluid">
+        <div class="row"> 
+            <div class="col text-center">
+                <img src="images\udemy.jpg">
+                <p>asdf1</p>
+            </div>
+
+            <div class="col text-center">
+                <img src="images\udemy.jpg">
+                <p>asdf2</p>
+            </div>
+
+            <div class="col text-center">
+                <img src="images\udemy.jpg">
+                <p>asdf3</p>
+            </div>
+            <div class="col text-right">
+                <img src="images\udemy.jpg">
+                <p>asdf3</p>
+            </div>
+        </div>
+    </div>        
+</div>
+
+@include('chat')
+    
+@include('bottom')
+     
+
+ 
